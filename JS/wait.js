143beb002b1003c34c3db3c122df8aff4ae9b094
anttondev <antton.chevrier@gmail.com>
2020-12-17 21:27:15 +0100

    first commit

---
+++ JS/wait.js
@@ -0,0 +1,11 @@
+window.onload = function()
+{
+  setTimeout(function()
+  {
+    document.getElementById("wait").style.display = "block";
+  }, 6000);
+}
+
+
+// var audio = new Audio('../audio/Jingle_Bell_Rock-Z0ajuTaHBtM.mp3');
+// audio.play();
